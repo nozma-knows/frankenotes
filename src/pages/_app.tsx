2fe581ee2f5c3fc42c1b6ce27324c9beecb50bd2
Noah Milberger <n.milberger@gmail.com>
2023-03-28 13:07:57 -0400

    Still working on getting google analytics to work.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,31 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Script from "next/script";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Analytics } from "@vercel/analytics/react";
+import * as gtag from "@/components/utils/analytics/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ApolloProvider client={client}>
       <Head>
         <title>Frankenotes</title>
       </Head>
-      {/* <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-      `}
-      </Script> */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
