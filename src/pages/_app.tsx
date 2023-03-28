0dd342138b468c52504d1dd00090a96d7b3467f1
Noah Milberger <n.milberger@gmail.com>
2023-03-28 13:18:36 -0400

    Still can not get it working. trying out nextjs-google-analytics npm library.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import Script from "next/script";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Analytics } from "@vercel/analytics/react";
-import * as gtag from "@/components/utils/analytics/gtag";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -26,25 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Frankenotes</title>
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
-
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <Analytics />
     </ApolloProvider>
