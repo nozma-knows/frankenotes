199de03ae62629b4dcbb8227e20b284f7bb76ede
Noah Milberger <n.milberger@gmail.com>
2023-03-28 19:41:00 -0400

    Updating google analytics script.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import "@/styles/globals.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Head from "next/head";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Analytics } from "@vercel/analytics/react";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -16,7 +16,25 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Frankenotes</title>
       </Head>
-      <GoogleAnalytics trackPageViews />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          tag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </ApolloProvider>
