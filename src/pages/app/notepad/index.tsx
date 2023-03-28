02c7d9e2c0b052812cbfa728314dc7c8ef1b5db3
Noah Milberger <n.milberger@gmail.com>
2023-03-28 16:18:55 -0400

    Updated Feedback Button to be only on app/notepad.

--- src/pages/app/notepad/index.tsx
+++ src/pages/app/notepad/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useQuery, Context } from "@apollo/client";
 import { parse } from "cookie";
+import { Tooltip } from "@mui/material";
+import { BsFillChatRightFill } from "react-icons/bs";
 import { Note } from "@/__generated__/graphql";
 import { NotesQuery } from "@/components/graph";
 import Page from "@/components/ui/pages/Page";
@@ -11,14 +13,11 @@ import useWindowSize, {
   lgScreenMax,
   xlScreenMin,
 } from "@/components/utils/hooks/useWindowSize";
-import Logo from "@/components/ui/icons/Logo";
-import FrankenotesLogo from "@/icons/logo.svg";
 import Editor from "@/components/feature-notepad/Editor";
 import NoteContext from "@/components/feature-notepad/context/useNoteContext";
 import LoadingPage from "@/components/ui/pages/LoadingPage";
 import ErrorPage from "@/components/ui/pages/ErrorPage";
-
-const title = `Frankenotes`;
+import FeedbackPopup from "@/components/ui/popups/FeedbackPopup";
 
 export async function getServerSideProps(context: Context) {
   const { token } = parse(context.req.headers.cookie);
@@ -28,9 +27,35 @@ export async function getServerSideProps(context: Context) {
   return { redirect: { destination: "/" } };
 }
 
+const FeedbackButton = ({
+  disabled,
+  setShowFeedbackPopup,
+}: {
+  disabled: boolean;
+  setShowFeedbackPopup: (showFeedbackPopup: boolean) => void;
+}) => {
+  return (
+    <div className="absolute bottom-4 right-4">
+      <Tooltip title="Share feedback!" arrow>
+        <button
+          disabled={disabled}
+          onClick={() => setShowFeedbackPopup(true)}
+          type="button"
+          aria-label="Share feedback!"
+          className={`${!disabled && "button"} bg-tertiary-dark p-2 rounded-lg`}
+        >
+          <BsFillChatRightFill className="p-2 w-12 h-12" />
+        </button>
+      </Tooltip>
+    </div>
+  );
+};
+
 export default function Notepad({ token }: { token: string }) {
   const decodedToken = DecodeToken({ token });
   const [activeNote, setActiveNote] = useState<Note | null>(null);
+
+  const [showFeedbackPopup, setShowFeedbackPopup] = useState(false);
 
   const authorId = decodedToken?.userId;
   const [fileManagerOpen, setFileManagerOpen] = useState(true);
@@ -96,7 +121,16 @@ export default function Notepad({ token }: { token: string }) {
         }}
       >
         <Page hideTopbar>
-          <Editor />
+          <>
+            {showFeedbackPopup && (
+              <FeedbackPopup onClose={() => setShowFeedbackPopup(false)} />
+            )}
+            <FeedbackButton
+              disabled={false}
+              setShowFeedbackPopup={setShowFeedbackPopup}
+            />
+            <Editor />
+          </>
         </Page>
       </NoteContext.Provider>
     );
