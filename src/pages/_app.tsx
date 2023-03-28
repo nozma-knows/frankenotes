39f929a50f6cf57de65ca83acf314ac62f538b05
Noah Milberger <n.milberger@gmail.com>
2023-03-28 12:46:52 -0400

    Updated script for google analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import Script from "next/script";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
@@ -7,6 +9,18 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>Frankenotes</title>
+      </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+      `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </ApolloProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,24 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-                page_path: window.location.pathname
-              });
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
