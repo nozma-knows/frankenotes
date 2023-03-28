f93f4ba39f7ae735bf54e8d5b8e201ceaf118a40
Noah Milberger <n.milberger@gmail.com>
2023-03-28 13:21:25 -0400

    Fixed bugs with build.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,6 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <ApolloProvider client={client}>
