05565cdf12b627aa16ee0e1abb06864f6b4ed77e
Noah Milberger <n.milberger@gmail.com>
2023-03-28 19:54:30 -0400

    Hopefully this works for getting google analytics working.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,31 @@
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import Script from "next/script";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Analytics } from "@vercel/analytics/react";
+import * as gtag from "@/components/utils/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ApolloProvider client={client}>
       <Head>
         <title>Frankenotes</title>
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-      />
-
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          tag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      />
       <Component {...pageProps} />
       <Analytics />
     </ApolloProvider>
