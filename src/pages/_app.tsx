ba2b211f584c51b9537406b7701c62093fe390af
Noah Milberger <n.milberger@gmail.com>
2023-03-28 11:55:41 -0400

    Added nextjs analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
+      <Analytics />
     </ApolloProvider>
   );
 }
