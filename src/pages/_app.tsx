ad71c935c3d4f12d869896d832051fbd1f618ac1
Noah Milberger <n.milberger@gmail.com>
2023-03-28 20:04:59 -0400

    Hopefully this works for getting google analytics working.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,22 @@ import "@/styles/globals.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Analytics } from "@vercel/analytics/react";
-import * as gtag from "@/components/utils/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    window.gtag = function () {
+      (window as any).dataLayer.push(arguments);
     };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+    window.gtag("js", new Date());
+    window.gtag("config", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+  }, []);
 
   return (
     <ApolloProvider client={client}>
@@ -27,6 +25,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Frankenotes</title>
       </Head>
       <Component {...pageProps} />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
       <Analytics />
     </ApolloProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,22 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
-        }}
-      />
       <body>
         <Main />
         <NextScript />
