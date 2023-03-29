9fcf92b0b305fcb36996afc00a9f4ce36f738c6d
Noah Milberger <n.milberger@gmail.com>
2023-03-29 10:48:05 -0400

    Small logo UI update.

--- src/components/feature-auth/ui/Topbar.tsx
+++ src/components/feature-auth/ui/Topbar.tsx
@@ -6,7 +6,7 @@ const title = `Frankenotes`;
 
 export default function Topbar() {
   return (
-    <div className="relative flex w-full items-center justify-center pt-12 px-8 h-20 text-main-dark">
+    <div className="relative flex w-full items-center justify-start sm:justify-center pt-12 px-8 h-20 text-main-dark">
       <Link href="/" className="flex items-center gap-2">
         <Logo Icon={FrankenotesLogo} text={title} />
       </Link>
